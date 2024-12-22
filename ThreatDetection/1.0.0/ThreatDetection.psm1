cb0597da052eca13fdaeff04a6b82580673d9ce2
zhazhalove <12418559+zhazhalove@users.noreply.github.com>
2024-12-22 13:42:29 -0500

    added additonal params (#5)

--- ThreatDetection/1.0.0/ThreatDetection.psm1
+++ ThreatDetection/1.0.0/ThreatDetection.psm1
@@ -1,3 +1,121 @@
+# <#
+# .SYNOPSIS
+#     Main function to invoke Threat Detection Score.
+
+# .DESCRIPTION
+#     This function orchestrates the validation of the micromamba environment and executes the Python script for Threat Detection Scoring.
+
+# .PARAMETER InputMessage
+#     The input message to pass to the Python script.
+
+# .PARAMETER Script
+#     Path to the Python script to execute. This is a mandatory parameter.
+
+# .PARAMETER PythonVersion
+#     The Python version to use within the micromamba environment. Defaults to 3.11.
+
+# .PARAMETER MicromambaEnvName
+#     The micromamba environment to use. Defaults to "langchain".
+
+# .PARAMETER MAMBA_ROOT_PREFIX
+#     The root prefix for the micromamba environment. Defaults to `$env:APPDATA`.
+
+# .PARAMETER Packages
+#     A string[] list of additional python packages to install into the mircomamba environment.
+
+#  .PARAMETER DotEnvPath     
+#     A string path to dotenv file containing environment variables to load into the runtime
+
+# .EXAMPLE
+#     Invoke-THDScore -InputMessage "Sample input message" -Script "path\to\script.py"
+
+# .EXAMPLE
+#     Invoke-THDScore -InputMessage "Sample input message" -Script "path\to\script.py" -PythonVersion "3.10"
+
+# .EXAMPLE
+#     Invoke-THDScore -InputMessage "Sample input message" -Script "path\to\script.py" -PythonVersion "3.10" -Packages @("numpy", "pandas", "matplotlib")
+
+# .EXAMPLE
+#     Invoke-THDScore -InputMessage "Sample input message" -Script "path\to\script.py" -PythonVersion "3.10" -Packages @("numpy", "pandas", "matplotlib") -DotEnvPath "C:\some\file\.env"
+# #>
+# function Invoke-THDScore {
+#     param (
+#         [Parameter(Mandatory = $true, HelpMessage = "Input Message")]
+#         [string]$InputMessage,
+
+#         [Parameter(Mandatory = $true, HelpMessage = "Path to the Python script to execute.")]
+#         [string]$Script,
+        
+#         [Parameter(Mandatory = $false, HelpMessage = "The Python version to use within the micromamba environment. Defaults to 3.11.")]
+#         [string]$PythonVersion = "3.11",
+        
+#         [Parameter(Mandatory = $false, HelpMessage = "The micromamba environment to use. Defaults to `"langchain`".")]
+#         [string]$MicromambaEnvName = "langchain",
+        
+#         [Parameter(Mandatory = $false, HelpMessage = "The micromamba root prefix to use. Defaults to `$env:APPDATA\micromamba.")]
+#         [string]$MAMBA_ROOT_PREFIX = "$env:APPDATA\micromamba",
+
+#         [Parameter(Mandatory = $false)]
+#         [string[]]$Packages,
+
+#         [Parameter(Mandatory = $false, HelpMessage = "Path to dotenv file containing environment variables to load into the runtime")]
+#         [string]$DotEnvPath
+#     )
+
+
+#     try {
+
+
+#         if ($null -ne $DotEnvPath ) {
+#             # Load .env variables into the PowerShell runtime
+#             Import-DotEnv -EnvFilePath $DotEnvPath | Out-Null
+#         }
+
+#         # Validate input - throws exception if fails
+#         Test-InputMessage -InputMessage $InputMessage
+
+#         # Set MAMBA_ROOT_PREFIX environment variable
+#         Initialize-MambaRootPrefix -MAMBA_ROOT_PREFIX $MAMBA_ROOT_PREFIX
+
+#         # Dowload micromamaba
+#         if ( -not (Get-MicromambaBinary) ) {
+#             throw [System.Exception]::new("FAIL - Download micromamba")
+#         }
+    
+#         # Build micromamba environment
+#         if ( -not (New-MicromambaEnvironment -EnvName $MicromambaEnvName -PythonVersion $PythonVersion) )
+#         {
+#             throw [System.Exception]::new("FAIL - Create micromamba environment - $MicromambaEnvName")
+#         }
+
+#         # Install micromamba environment python packages
+#         $PkgResults = Install-PackagesInMicromambaEnvironment -EnvName $MicromambaEnvName -Packages $Packages
+
+#         foreach($result in $PkgResults) {
+#             if (-not $result["Success"]) {
+#                 throw [System.Exception]::new("$($result["PackageName"]) $($result["Success"])")
+#             }
+#         }
+        
+#         # Execute the Python script
+#         $thdResult = Invoke-PythonScript -ScriptPath $Script -EnvName $MicromambaEnvName -Arguments "-i $InputMessage"
+
+#         if ($null -ne $thdResult) {
+#             return $thdResult
+#         } else {
+#             return "Failed to retrieve result from the Python script."
+#         }
+#     }
+#     catch {
+#         throw [System.Exception]::new("Error in Invoke-THDScore: $($_.Exception.Message)", $_.Exception)
+#     }
+#     finally {
+#         Remove-MicromambaEnvironment -EnvName $MicromambaEnvName | Out-Null
+#         Remove-Micromamba | Out-Null
+#     }
+# }
+
+
 <#
 .SYNOPSIS
     Main function to invoke Threat Detection Score.
@@ -21,10 +139,19 @@
     The root prefix for the micromamba environment. Defaults to `$env:APPDATA`.
 
 .PARAMETER Packages
-    A string[] list of additional python packages to install into the mircomamba environment.
+    A string[] list of additional Python packages to install into the micromamba environment.
 
- .PARAMETER DotEnvPath     
-    A string path to dotenv file containing environment variables to load into the runtime
+.PARAMETER DotEnvPath
+    A string path to a dotenv file containing environment variables to load into the runtime.
+
+.PARAMETER Temperature
+    The temperature of the LLM. Defaults to 0.0.
+
+.PARAMETER ModelName
+    The LLM model used. Defaults to "gpt-4o-mini".
+
+.PARAMETER MaxRetries
+    The maximum number of retries for the LLM. Defaults to 3.
 
 .EXAMPLE
     Invoke-THDScore -InputMessage "Sample input message" -Script "path\to\script.py"
@@ -37,6 +164,9 @@
 
 .EXAMPLE
     Invoke-THDScore -InputMessage "Sample input message" -Script "path\to\script.py" -PythonVersion "3.10" -Packages @("numpy", "pandas", "matplotlib") -DotEnvPath "C:\some\file\.env"
+
+.EXAMPLE
+    Invoke-THDScore -InputMessage "Sample input message" -Script "path\to\script.py" -Temperature 0.7 -ModelName "gpt-4" -MaxRetries 5
 #>
 function Invoke-THDScore {
     param (
@@ -45,13 +175,13 @@ function Invoke-THDScore {
 
         [Parameter(Mandatory = $true, HelpMessage = "Path to the Python script to execute.")]
         [string]$Script,
-        
+
         [Parameter(Mandatory = $false, HelpMessage = "The Python version to use within the micromamba environment. Defaults to 3.11.")]
         [string]$PythonVersion = "3.11",
-        
-        [Parameter(Mandatory = $false, HelpMessage = "The micromamba environment to use. Defaults to `"langchain`".")]
+
+        [Parameter(Mandatory = $false, HelpMessage = "The micromamba environment to use. Defaults to `langchain`.")]
         [string]$MicromambaEnvName = "langchain",
-        
+
         [Parameter(Mandatory = $false, HelpMessage = "The micromamba root prefix to use. Defaults to `$env:APPDATA\micromamba.")]
         [string]$MAMBA_ROOT_PREFIX = "$env:APPDATA\micromamba",
 
@@ -59,14 +189,21 @@ function Invoke-THDScore {
         [string[]]$Packages,
 
         [Parameter(Mandatory = $false, HelpMessage = "Path to dotenv file containing environment variables to load into the runtime")]
-        [string]$DotEnvPath
-    )
+        [string]$DotEnvPath,
 
+        [Parameter(Mandatory = $false, HelpMessage = "The temperature of the LLM. Defaults to 0.0.")]
+        [float]$Temperature = 0.0,
+
+        [Parameter(Mandatory = $false, HelpMessage = "The LLM model used. Defaults to 'gpt-4o-mini'.")]
+        [string]$ModelName = "gpt-4o-mini",
+
+        [Parameter(Mandatory = $false, HelpMessage = "The maximum number of retries for the LLM. Defaults to 3.")]
+        [int]$MaxRetries = 3
+    )
 
     try {
 
-
-        if ($null -ne $DotEnvPath ) {
+        if ($null -ne $DotEnvPath) {
             # Load .env variables into the PowerShell runtime
             Import-DotEnv -EnvFilePath $DotEnvPath | Out-Null
         }
@@ -77,43 +214,54 @@ function Invoke-THDScore {
         # Set MAMBA_ROOT_PREFIX environment variable
         Initialize-MambaRootPrefix -MAMBA_ROOT_PREFIX $MAMBA_ROOT_PREFIX
 
-        # Dowload micromamaba
-        if ( -not (Get-MicromambaBinary) ) {
+        # Download micromamba
+        if (-not (Get-MicromambaBinary)) {
             throw [System.Exception]::new("FAIL - Download micromamba")
         }
-    
+
         # Build micromamba environment
-        if ( -not (New-MicromambaEnvironment -EnvName $MicromambaEnvName -PythonVersion $PythonVersion) )
-        {
+        if (-not (New-MicromambaEnvironment -EnvName $MicromambaEnvName -PythonVersion $PythonVersion)) {
             throw [System.Exception]::new("FAIL - Create micromamba environment - $MicromambaEnvName")
         }
 
-        # Install micromamba environment python packages
-        $PkgResults = Install-PackagesInMicromambaEnvironment -EnvName $MicromambaEnvName -Packages $Packages
+        # Check if Packages parameter is provided
+        if ($null -ne $Packages -and $Packages.Count -gt 0) {
 
-        foreach($result in $PkgResults) {
-            if (-not $result["Success"]) {
-                throw [System.Exception]::new("$($result["PackageName"]) $($result["Success"])")
+            # Install micromamba environment Python packages
+            $PkgResults = Install-PackagesInMicromambaEnvironment -EnvName $MicromambaEnvName -Packages $Packages
+
+            foreach ($result in $PkgResults) {
+
+                if (-not $result["Success"]) {
+                    throw [System.Exception]::new("$($result["PackageName"]) $($result["Success"])")
+                }
             }
         }
-        
-        # Execute the Python script
-        $thdResult = Invoke-PythonScript -ScriptPath $Script -EnvName $MicromambaEnvName -Arguments "-i $InputMessage"
+
+        # Execute the Python script with new parameters
+        $arguments = @(
+            "-i", $InputMessage,
+            "--temperature", $Temperature,
+            "--model-name", $ModelName,
+            "--max-retries", $MaxRetries
+        )
+
+        $thdResult = Invoke-PythonScript -ScriptPath $Script -EnvName $MicromambaEnvName -Arguments $arguments
 
         if ($null -ne $thdResult) {
             return $thdResult
         } else {
             return "Failed to retrieve result from the Python script."
         }
-    }
-    catch {
+        
+    } catch {
         throw [System.Exception]::new("Error in Invoke-THDScore: $($_.Exception.Message)", $_.Exception)
-    }
-    finally {
+    } finally {
         Remove-MicromambaEnvironment -EnvName $MicromambaEnvName | Out-Null
         Remove-Micromamba | Out-Null
     }
 }
+
 
 
 <#
